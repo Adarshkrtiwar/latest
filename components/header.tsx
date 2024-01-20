cc51c2598a20914a8ad9924ba981ca39cfef0a64
thisissuman <43521912+thisissuman@users.noreply.github.com>
2024-01-20 21:58:31 +0530

    fix

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
-import { navbarAnime } from "./Base/BaseAnimation";
 
 import { useActiveSectionContext } from "@/context/active-session-context";
 
@@ -20,11 +19,7 @@ export default function Header() {
         initial={{ y: -200, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <motion.div
-      /* variants={navbarAnime}
-initial="from"
-animate="to" */
-      >
+      <motion.div      >
         <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0  cursor-none hover:highlight">
           <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5  cursor-none hover:highlight">
             {links.map((link) => (
